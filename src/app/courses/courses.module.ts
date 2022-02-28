b0b40c8773acc590e5783d458c7320f568d5aaeb
Bruno Melo <br.lopesmelo@gmail.com>
2022-02-28 00:18:26 -0300

    adicionando tabela de cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule, MatCardModule, MatToolbarModule],
 })
 export class CoursesModule {}
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  public courses: Course[] = [{
+    _id: 'a', name: 'Angular', category: 'front-end'
+  }];
+  public displayedColumns: string[] = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
