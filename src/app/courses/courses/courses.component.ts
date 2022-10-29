523f687891547c7db352c861bd15858b96ccadd2
Bruno Melo <br.lopesmelo@gmail.com>
2022-10-29 18:53:10 -0300

    Aula 16 - implementa proxy e ajusta componente de courses para receber dados da api

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -13,16 +13,16 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   public courses$: Observable<Course[]>;
-  public displayedColumns: string[] = ['name', 'category'];
+  public displayedColumns: string[] = ['ID','name', 'category'];
 
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog
   ) {
-    this.courses$ = coursesService.list().pipe(
+    this.courses$ = this.coursesService.list().pipe(
       catchError((error) => {
         this.onError('Erro ao carregar cursos.')
-        console.log(error);
+        console.log("error", error);
         return of([]);
       })
     );
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -7,14 +7,14 @@ import { first, tap, delay } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = 'localhost:8080/api/courses';
+  private readonly API = '/api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(5000),
+      delay(1000),
       tap((courses: any) => console.log(courses))
     );
   }
